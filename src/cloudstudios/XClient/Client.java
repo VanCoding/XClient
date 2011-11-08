95aaeefb510285910ec2f7c3f937a865611f0926
Patrik Stutz <patrik.stutz@gmail.com>
2011-11-08 15:23:39 +0100

    added dynamic channel count detection

--- src/cloudstudios/XClient/Client.java
+++ src/cloudstudios/XClient/Client.java
@@ -5,25 +5,48 @@ import java.util.ArrayList;
 
 public class Client {
 	private Socket socket;
-	private ArrayList<Channel> channels;
+	private ArrayList<Channel> inputchannels = new ArrayList<Channel>();
+	private ArrayList<Channel> outputchannels = new ArrayList<Channel>();;
 	private int device;
 	
 	public Client(String ip, int device){
 		this.device = device;
 		try {
 			socket = new Socket(ip,10001);
-			channels = new ArrayList<Channel>();
-			for(int i = 0; i < 16; i++){
-				channels.add(new Channel(socket,this.device,i < 8,i<8?i:i-8));
+			Command cmd;
+			
+			//load input channels
+			cmd = new Command(true,"%nI0");
+			cmd.setDevice(this.device);
+			socket.getOutputStream().write(cmd.getBytes());
+			cmd = new Command(socket);
+			for(int i = 0; i < cmd.getData(); i++){
+				inputchannels.add(new Channel(socket,this.device,true,i));
+			}
+			
+			//load output channels
+			cmd = new Command(true,"%nO0");
+			cmd.setDevice(this.device);
+			socket.getOutputStream().write(cmd.getBytes());
+			cmd = new Command(socket);
+			for(int i = 0; i < cmd.getData(); i++){
+				outputchannels.add(new Channel(socket,this.device,true,i));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}	
+	}
+	
 	public Channel getInputChannel(int i){
-		return channels.get(i);
+		return inputchannels.get(i);
 	}
 	public Channel getOutputChannel(int i){
-		return channels.get(i+8);
+		return outputchannels.get(i+8);
+	}
+	public int getInputChannelCount(){
+		return inputchannels.size();
+	}
+	public int getOutputChannelCount(){
+		return outputchannels.size();
 	}
 }
